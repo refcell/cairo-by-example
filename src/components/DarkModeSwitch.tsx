3b9f074634497c9f337d1c7a0cbcfdb613af3524
Alucard <ladonseth1@gmail.com>
2021-12-24 02:15:07 -0700

    toogle switch

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,16 +1,37 @@
-import { useColorMode, Switch } from '@chakra-ui/react'
+import { useColorMode, Switch, Box, Image } from '@chakra-ui/react'
 
-export const DarkModeSwitch = () => {
+const DarkModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode()
   const isDark = colorMode === 'dark'
   return (
-    <Switch
-      position="fixed"
-      top="1rem"
-      right="1rem"
-      color="green"
-      isChecked={isDark}
-      onChange={toggleColorMode}
-    />
+    <Box>
+      {isDark ? (
+        <Image
+          src={"/mode-light.svg"}
+          alt="light mode"
+          position="fixed"
+          top="1rem"
+          right="1rem"
+          color="green"
+          w='30px'
+          cursor='pointer'
+          onClick={(e) => toggleColorMode()}
+        />
+      ) : (
+        <Image
+          src={"/mode-dark.svg"}
+          alt="dark mode"
+          position="fixed"
+          top="1rem"
+          right="1rem"
+          color="green"
+          w='30px'
+          cursor='pointer'
+          onClick={(e) => toggleColorMode()}
+        />
+      )}
+    </Box>
   )
 }
+
+export default DarkModeSwitch;
