a8cea9d85329487eb7a3745b578d19d8395429cd
Alucard <ladonseth1@gmail.com>
2021-12-28 08:44:21 -0700

    undo pwa in prod for next config

--- next.config.js
+++ next.config.js
@@ -5,10 +5,9 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     disable:
-      process.env.NODE_ENV === "development", // ||
-      // process.env.NODE_ENV === "preview" ||
-      // process.env.NODE_ENV === "production",
-      // lines above commented to enable pwa in production
+      process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "preview" ||
+      process.env.NODE_ENV === "production",
     dest: "public",
     register: true,
   },
